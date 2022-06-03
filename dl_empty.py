22156cd57764edae0832029e6653ed46e6bd8b42
NathanPerrine <nathan.perrine@gmail.com>
2022-06-03 01:05:26 -0400

    thurs night, move file

--- dl_empty.py
+++ dl_empty.py
@@ -2,8 +2,9 @@ import os
 import shutil 
 
 downloads = os.listdir('D:\Downloads')
-to_move = r'D:\Downloads\pexels-photo-2885320.jpeg'
-wallpapers = r'D:\Nathan\Pictures\Wallpapers'
+# to_move = r'D:\Downloads\pexels-photo-2885320.jpeg'
+to_move = 'D:/Downloads/"blank - Copy (5).txt"'
+wallpapers = r'D:\Nathan\Documents'
 
 # for dl in downloads:
 #     print(dl)
--- dl_gui.py
+++ dl_gui.py
@@ -1,20 +1,19 @@
 from tkinter.messagebox import YES
-from typing import List
-from tkinter import filedialog
+from typing import Dict, List
+from tkinter import END, filedialog
 import tkinter as tk
 import os
+import shutil
 import json
 
 
-def load_from_json(json_file):
+def load_from_json(json_file) -> Dict:
     with open(json_file, 'r') as f:
         data = json.load(f)
     return data
 
 
 def create_json_file():
-    # TODO: Create json
-    #   
 
     tk.messagebox.showinfo(title="Select Downloads", message="Select your download folder from the explorer.")
     dl_path = tk.filedialog.askdirectory()
@@ -23,25 +22,35 @@ def create_json_file():
         with open("./folders.json", "w", encoding="utf-8") as f:
             data = {
                 "download": dl_path,
-                "destinations" : [] 
+                "destinations" : {}
             }
             json.dump(data, f, ensure_ascii=False, indent=4)
     else:
         create_json_file()
 
 
-def move_file(from_box, to_box, file):
+def move_file(from_box, from_info, dl_direct, to_box, to_info, info):
     # TODO: move_file
     #   Move file to new directory
     #   Reset downloads list
     #   Display what moved to where
-    # selected_files = dl_list_box.curselection()
-    pass
+    #   Check if nothing is selected in either box
+
+    selected_files = from_box.curselection()
+    selected_dest = to_box.get(to_box.curselection())
+    print(f"{to_info=}")
+
+    for f in selected_files:
+        file_to_move = f'{dl_direct}/{from_info[f]}'
+        print(f"{file_to_move} -> {to_info[selected_dest]} ")
+        shutil.move(r'{}'.format(file_to_move), to_info[selected_dest])
+
+    from_info = os.listdir(info['download'])
+    fill_list_box(from_box, from_info)
 
 
 def fill_list_box(box, info: List) -> None:
-    # TODO: fill_list_box
-    #   
+    box.delete(0, END)
     for i, v in enumerate(info):
         box.insert(i, v)
 
@@ -49,11 +58,13 @@ def fill_list_box(box, info: List) -> None:
 def add_destination(box):
     print("hello world")
     new_dest = filedialog.askdirectory()
+    dest_title = new_dest.split("/")[-1]
 
     if new_dest:
         with open('./folders.json', 'r+') as f:
             data = json.load(f)
-            data['destinations'].append(new_dest)
+            # data['destinations'].append(new_dest)
+            data['destinations'][dest_title] = f"{new_dest}/"
             f.seek(0)
             json.dump(data, f, ensure_ascii=False, indent=4)
     
@@ -82,7 +93,7 @@ def main():
 
     # DL Listbox
     dl_list = os.listdir(info['download'])
-    dl_list_box = tk.Listbox(dl_frame, selectmode="multiple")
+    dl_list_box = tk.Listbox(dl_frame, selectmode="multiple", exportselection=0)
     dl_list_box.pack(padx=10, pady=10)
     fill_list_box(dl_list_box, dl_list)
 
@@ -94,7 +105,7 @@ def main():
     arrow_frame.grid(row=0, column=3, padx=5)
 
     # Arrow Button
-    arrow_button = tk.Button(arrow_frame, text="->")
+    arrow_button = tk.Button(arrow_frame, text="->", command= lambda: move_file(dl_list_box, dl_list, info['download'], destination_list_box, info['destinations'], info))
     arrow_button.pack()
 
 
@@ -104,7 +115,7 @@ def main():
     destination_frame.grid(row=0, column=4, padx=20, pady=20)
 
     # Dest. Listbox 
-    destination_list_box = tk.Listbox(destination_frame, width=25)
+    destination_list_box = tk.Listbox(destination_frame, width=25, exportselection=0)
     destination_list_box.grid(row=0, column=0, padx=10, pady=10)
     fill_list_box(destination_list_box, info['destinations'])
     
@@ -114,6 +125,10 @@ def main():
     # Add destination button
     add_button = tk.Button(dest_button_frame, text="+", command= lambda: add_destination(destination_list_box))
     add_button.pack()
+
+
+    # Arrow button command after frames loaded
+    # arrow_button.config(command= lambda: move_file(dl_list_box, destination_list_box))
 
     ### Main Loop ### 
     window.mainloop()
